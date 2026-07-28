13bc31db4b2f46fe9c347c0ba2453ca02aad6230
violin788788 <oak234345@gmail.com>
2026-07-28 11:39:45 -0400

    sync 2026-07-28_11-39-45

--- ebook_code/pdf_convert.py
+++
@@ -1,4 +0,0 @@
-from pdf2image import convert_from_path
-
-# Convert all 200 pages directly to JPEG
-convert_from_path('document.pdf', output_folder='extracted_images', fmt='jpeg')
--- ebook_code/pdf_crop_pdf.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python "pdf_crop_pdf.py"
-pause
