5e3d97fb8be25c68f3e44ac5bf40b593ebeac1f6
violin788788 <oak234345@gmail.com>
2026-07-29 18:22:39 -0400

    sync 2026-07-29_18-22-38

--- ebook_code/0mp3_and_song.py
+++ ebook_code/0mp3_and_song.py
@@ -1,3 +1,36 @@
+
+
+import os,gc
+from pydub import AudioSegment
+from datetime import datetime
+AudioSegment.converter=r".\ffmpeg.exe"
+AudioSegment.ffprobe=r".\ffprobe.exe"
+song="dmitri.mp3"
+audio_book_folder="grant"
+start_file=6
+end_file=12
+sound1=AudioSegment.from_mp3(song)
+for a in range(start_file,end_file+1):
+    start_time=datetime.now()
+    narrate_file=os.path.join(audio_book_folder,"part_"+str(a)+".mp3")
+    output_file=narrate_file.replace("part","0"+audio_book_folder+"_part")
+    print("generating")
+    print(output_file)
+    sound2=AudioSegment.from_mp3(narrate_file)
+    if len(sound1)>len(sound2):
+        narration,bg_music=sound1,sound2
+    else:
+        narration,bg_music=sound2,sound1
+    bg_music=bg_music-15
+    mixed_sound=narration.overlay(bg_music,loop=True)
+    mixed_sound.export(output_file,format="mp3")
+    del sound2,narration,bg_music,mixed_sound
+    gc.collect()
+    end_time=datetime.now()
+    print("time to gen file =",end_time-start_time)
+os.startfile(os.getcwd())
+
+"""
 import sys,os
 #new_path = os.path.join(a,b,c)
 #cwd = os.getcwd()
@@ -38,3 +71,5 @@ for a in range(start_file,end_file+1):
     time_difference = end_time - start_time
     print("time to gen file = ", time_difference)
     os.startfile(os.getcwd())
+
+    """
