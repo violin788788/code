b3e2de3efe39a57f4a384b22163f4e793c8552a9
violin788788 <oak234345@gmail.com>
2026-07-29 18:39:38 -0400

    sync 2026-07-29_18-39-38

--- ebook_code/0mp3_and_song.py
+++ ebook_code/0mp3_and_song.py
@@ -1,4 +1,53 @@
 
+import os
+
+from pydub import AudioSegment
+AudioSegment.converter=r".\ffmpeg.exe"
+AudioSegment.ffprobe=r".\ffprobe.exe"
+
+audio1=AudioSegment.from_mp3("dmitri.mp3")-15
+directory = "grant"
+start_file = 6
+end_file = 12
+
+for a in range(start_file,end_file):
+    print(directory,start_file,end_file)
+    #print(a)
+    part_file = os.path.join(directory,"part_"+str(a)+".mp3")
+    audio2=AudioSegment.from_mp3(part_file)
+    #mixed=audio1.overlay(audio2,loop=True)
+    mixed=audio2.overlay(audio1,loop=True)
+    output_file = os.path.join(directory,"grant_"+str(a)+".mp3")
+    mixed.export(output_file,format="mp3")
+    #print(a)
+
+
+
+
+"""
+
+import os,subprocess
+from datetime import datetime
+ffmpeg=r".\ffmpeg.exe"
+song="dmitri.mp3"
+audio_book_folder="grant"
+start_file=6
+end_file=12
+for a in range(start_file,end_file+1):
+    start_time=datetime.now()
+    narrate_file=os.path.join(audio_book_folder,"part_"+str(a)+".mp3")
+    output_file=narrate_file.replace("part","0"+audio_book_folder+"_part")
+    print("generating")
+    print(output_file)
+    subprocess.run([ffmpeg,"-y","-i",narrate_file,"-stream_loop","-1","-i",song,"-filter_complex","[1:a]volume=0.18[bg];[0:a][bg]amix=inputs=2:duration=first","-c:a","libmp3lame","-q:a","2",output_file],check=True)
+    end_time=datetime.now()
+    print("time to gen file =",end_time-start_time)
+os.startfile(os.getcwd())
+
+
+
+
+
 
 import os,gc
 from pydub import AudioSegment
@@ -30,7 +79,7 @@ for a in range(start_file,end_file+1):
     print("time to gen file =",end_time-start_time)
 os.startfile(os.getcwd())
 
-"""
+
 import sys,os
 #new_path = os.path.join(a,b,c)
 #cwd = os.getcwd()
