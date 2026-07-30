21ca08be18360f94c292ce8687ebf352ffe4f5b6
Your Name <you@example.com>
2026-07-30 19:45:02 +0000

    sync 2026-07-30_19-45-02

--- ebook_code/txt_to_epub.py
+++ ebook_code/txt_to_epub.py
@@ -1,11 +1,14 @@
 from ebooklib import epub
 from pathlib import Path
 import html
-
+import zipfile
+import tempfile
+import shutil
+import os
 txtfile="war_and_national_finance.txt"
-title=txtfile.replace(".txt","")
+title="War and National Finance"
 author="brand"
-
+coverfile="war_and_national_finance.png"
 text=Path(txtfile).read_text(encoding="utf-8")
 body="<p>"+html.escape(text).replace("\n\n","</p><p>").replace("\n","<br/>")+"</p>"
 book=epub.EpubBook()
@@ -13,13 +16,29 @@ book.set_identifier(Path(txtfile).stem)
 book.set_title(title)
 book.set_language("en")
 book.add_author(author)
+cover=epub.EpubItem(uid="cover-image",file_name=coverfile,media_type="image/png",content=Path(coverfile).read_bytes())
+book.add_item(cover)
+cover_page=epub.EpubHtml(title="Cover",file_name="cover.xhtml")
+cover_page.content=f'<html xmlns="http://www.w3.org/1999/xhtml"><body><img src="{coverfile}" alt="Cover"/></body></html>'
+book.add_item(cover_page)
 chapter=epub.EpubHtml(title=title,file_name="index.xhtml")
-chapter.content=f"<html><body><h1>{html.escape(title)}</h1>{body}</body></html>"
+chapter.content=f"<html xmlns=\"http://www.w3.org/1999/xhtml\"><body><h1>{html.escape(title)}</h1>{body}</body></html>"
 book.add_item(chapter)
-book.toc=(chapter,)
+book.toc=(cover_page,chapter)
 book.add_item(epub.EpubNcx())
 book.add_item(epub.EpubNav())
-book.spine=["nav",chapter]
+book.spine=[cover_page,chapter]
 outfile=Path(txtfile).with_suffix(".epub")
 epub.write_epub(str(outfile),book)
+tmp=outfile.with_suffix(".tmp.epub")
+with zipfile.ZipFile(outfile,"r") as zin,zipfile.ZipFile(tmp,"w") as zout:
+    for item in zin.infolist():
+        data=zin.read(item.filename)
+        if item.filename.endswith(".opf"):
+            opf=data.decode("utf-8")
+            opf=opf.replace(f'<item id="cover-image" href="{coverfile}" media-type="image/png"/>',f'<item id="cover-image" href="{coverfile}" media-type="image/png" properties="cover-image"/>')
+            opf=opf.replace("<metadata>","<metadata><meta name=\"cover\" content=\"cover-image\"/>")
+            data=opf.encode("utf-8")
+        zout.writestr(item,data)
+shutil.move(tmp,outfile)
 print(f"Created {outfile}")
