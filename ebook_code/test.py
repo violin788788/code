2e92a3188b85095d09bcbc325d079f2b5100725f
violin788788 <oak234345@gmail.com>
2026-08-03 12:50:45 -0400

    sync 2026-08-03_12-50-44

--- ebook_code/test.py
+++ ebook_code/test.py
@@ -1,8 +1,29 @@
+
+
+
 from utils import *
 import asyncio
 import edge_tts
 
 
+from pathlib import Path
+
+# 1. Define the original file path
+file_path = Path("document.txt")
+
+# 2. Generate the new path with the updated suffix
+new_path = file_path.with_suffix(".pdf")
+
+
+print(new_path)
+sys.exit()
+
+# 3. Rename the file on the actual hard drive
+if file_path.exists():
+    file_path.rename(new_path)
+    print(f"Renamed to: {new_path}")
+
+"""
 file = "french_revolution.txt"
 len_portion = 55000
 
@@ -37,3 +58,5 @@ for a in range(0,parts):
     # Run the async function
     asyncio.run(generate_tts())
     #os.startfile(output_file)
+
+"""
--- ebook_code/tts.py
+++ ebook_code/tts.py
@@ -2,8 +2,37 @@ from utils import *
 #from piper import PiperVoice
 import wave,epub2txt,platform
 from pathlib import Path
-def tts_win7():
-    blank = "blank"
+def tts_win7(txt_file):
+    minutes_per_file = 60
+    text = read_txt(txt_file)
+    words = text.split()
+    #voice = PiperVoice.load("en_US-lessac-medium.onnx")
+    #voice = PiperVoice.load("en_US-ryan-medium.onnx")
+    voice = "en-US-AriaNeural"
+    print("len(words)",len(words))
+    words_per_file =minutes_per_file*150
+    quit = 0
+    count = 0
+    while(quit<1):
+        count = count+1
+        begin = words_per_file*count
+        end = words_per_file*(count+1)
+        if end>len(words):
+            quit=1
+            end=len(words)
+        words = text[begin:end]
+        #output_file = txt_file.replace(".txt","_part"+str(count)+".wav")
+        ending = "_part"+str(count)+".mp3"
+        output_file = txt_file.replace(".txt",ending)
+        print("generating ",output_file)
+        async def run_tts():
+            communicate = edge_tts.Communicate(words, voice)
+            await communicate.save(output_file)
+        asyncio.run(run_tts())
+
+        #with wave.open(output_file, "wb") as wav_file:
+        #    voice.synthesize_wav(text, wav_file)
+        #print("Done - output.wav created")
 def tts_linux(txt_file):
     minutes_per_file = 60
     text = read_txt(txt_file)
@@ -56,21 +85,23 @@ convert_to_txt = {}
 convert_to_txt[".txt"] = ""
 convert_to_txt[".epub"] = epub_to_txt
 convert_to_txt[".pdf"] = pdf_to_txt
-gen_function = {}
-gen_function["Linux"] = tts_win7
-gen_function["Windows"] = tts_linux
+os_identify = {}
+os_identify["Windows"] = tts_win7
+os_identify["Linux"] = tts_linux
 
 file_to_generate = "french_revolution.txt"
+file_path = Path(file_to_generate)
+txt_file = file_path.with_suffix(".txt")
 
-txt_file = ""
 file_type = Path(file_to_generate).suffix
 for key, value in convert_to_txt.items():
     if file_type == key:
         if callable(value):
             txt_file = value(file_to_generate)
+
 system = platform.system()
 print(system)
-for key, value in gen_function.items():
+for key, value in os_identify.items():
     if system == key:
         if callable(value):
             value(txt_file)
