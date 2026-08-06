78cbdaffe8f6ce138dfb666058de2305d71eb4df
violin788788 <oak234345@gmail.com>
2026-08-06 18:08:31 -0400

    sync 2026-08-06_18-08-31

--- ebook_code/tts.py
+++ ebook_code/tts.py
@@ -81,34 +81,29 @@ def txt_to_mp3(directory,txt_file):
 
 
 def add_song(directory,song_file):
-    #add_song("common_sense","dmitri.mp3"):
-    import os
-    import time
-    import threading
-    from pydub import AudioSegment
-    #directory="common_sense"
-    #song_file="dmitri.mp3"
-    audio1=AudioSegment.from_mp3(song_file)
+    main_file=os.path.abspath(song_file)
     for val in os.listdir(directory):
-        if song_file in val:
+        if val==song_file:
+            continue
+        if not val.lower().endswith(".mp3"):
             continue
         part_file=os.path.join(directory,val)
-        audio2=AudioSegment.from_mp3(part_file)
-        if len(audio1)>len(audio2):
-            loops=(len(audio1)//len(audio2))+1
-            audio2=(audio2*loops)[:len(audio1)]
-        elif len(audio2)>len(audio1):
-            loops=(len(audio2)//len(audio1))+1
-            audio1=(audio1*loops)[:len(audio2)]
-        combined=audio1.overlay(audio2)
-        output_file=part_file.replace(".mp3","_"+song_file)
-        print("creating",output_file)
-        combined.export(output_file,format="mp3")
-        thread.join()
-        print("saved",output_file,"length",len(combined)//1000,"seconds")
+        output_file=part_file.replace(".mp3","_mixed.mp3")
+        print("mixing",part_file)
+        command=[
+            "ffmpeg",
+            "-y",
+            "-i",main_file,
+            "-i",part_file,
+            "-filter_complex",
+            "[0:a]aloop=loop=-1:size=2e+09[a0];[1:a]aloop=loop=-1:size=2e+09[a1];[a0][a1]amix=inputs=2:duration=longest",
+            "-c:a","libmp3lame",
+            "-q:a","2",
+            output_file
+        ]
+        subprocess.run(command,stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
+        print("saved",output_file)
     os.startfile(directory)
-
-
 
 
 main()
