d047cea36f180678eb8dcb0621af03341e8efac6
violin788788 <oak234345@gmail.com>
2026-08-06 16:25:09 -0400

    sync 2026-08-06_16-25-09

--- ebook_code/tts.py
+++ ebook_code/tts.py
@@ -1,52 +1,120 @@
 #from utils import *
 #from ..utils import *
-import wave,epub2txt,platform
+import wave,epub2txt,platform,os 
 from pathlib import Path
 def main():
-    file_to_generate = "ten_days.pdf"
+    #original_file = "rothschild_1798_1848.pdf"
+    original_file = "grant.epub"
 
     functions_to_run = []
-    functions_to_run.append([".pdf","pdf_function"])
-    functions_to_run.append([".epub","epub_function"])
-    txt_file = file_to_generate
+    functions_to_run.append([".pdf",pdf_to_txt])
+    functions_to_run.append([".epub",epub_to_txt])
+    txt_file = original_file
     for a in range(0,len(functions_to_run)):
         check = functions_to_run[a][0]
-        if check in file_to_generate:
-            #run thing
-            txt_file = txt_file.replace(check,".txt")
+        if check in original_file:
+            run = functions_to_run[a][1]
+            run(original_file)
+            txt_file = original_file.replace(check,".txt")
             break
+    os.startfile(txt_file)
+    txt_to_mp3(txt_file)
     #convert txt file to mp3s
     #that's it!
+def pdf_to_txt(pdf):
+    import fitz
+    doc=fitz.open(pdf)
+    print("pages:",len(doc))
+    text=""
+    for i,page in enumerate(doc):
+        print("getting page",i+1,"of",len(doc))
+        text+=page.get_text()+"\n"
+    output_file=pdf.replace(".pdf",".txt")
+    with open(output_file,"w",encoding="utf-8") as f:
+        f.write(text)
+    print("generated",output_file)
+def epub_to_txt(epub_file):
+    #epub_to_txt("french_revolution.epub")
+    import epub2txt
+    txt_file = epub_file.replace(".epub",".txt")
+    text_content = epub2txt.epub2txt(epub_file)
+    with open(txt_file, "w", encoding="utf-8") as f:
+        f.write(text_content)
+    print(f"Successfully converted {epub_file} to {txt_file}!")
+    return txt_file
+def txt_to_mp3(txt_file):
+    #txt_to_mp3(txt_file)
+    import os
+    import asyncio
+    import edge_tts
+    minutes_per_file = 60
+    with open(txt_file,"r",encoding="utf-8") as f:
+        text=f.read()
+    words = text.split()
+    #voice = PiperVoice.load("en_US-lessac-medium.onnx")
+    #voice = PiperVoice.load("en_US-ryan-medium.onnx")
+    voice = "en-US-AriaNeural"
+    print("len(words)",len(words))
+    words_per_file =minutes_per_file*150
+    parts = int(len(words)/words_per_file)
+    quit = 0
+    count = 0
+    while(quit<1):
+        count = count+1
+        begin = words_per_file*count
+        end = words_per_file*(count+1)
+        if end>len(words):
+            quit=1
+            end=len(words)
+        chunk_text = text[begin:end]
+        #output_file = txt_file.replace(".txt","_part"+str(count)+".wav")
+        ending = "_part"+str(count)+".mp3"
+        output_file = txt_file.replace(".txt",ending)
+        print("generating ",output_file,"of",parts)
+        async def run_tts():
+            communicate = edge_tts.Communicate(chunk_text, voice)
+            await communicate.save(output_file)
+        asyncio.run(run_tts())
+
+        #with wave.open(output_file, "wb") as wav_file:
+        #    voice.synthesize_wav(text, wav_file)
+        #print("Done - output.wav created")
+    
+
+        """
+        with open(txt_file,"r",encoding="utf-8") as f:
+            text=f.read()
+        output_file=txt_file.replace(".txt",".mp3")
+        async def generate():
+            voice="en-US-AriaNeural"
+            communicate=edge_tts.Communicate(text,voice)
+            await communicate.save(output_file)
+        asyncio.run(generate())
+        print("generated",output_file)
+        os.startfile(output_file)
+    
 
 
-    convert_to_txt = {}
-    convert_to_txt[".txt"] = ""
-    convert_to_txt[".epub"] = epub_to_txt
-    convert_to_txt[".pdf"] = pdf_to_txt
-    os_identify = {}
-    os_identify["Windows"] = tts_win7
-    os_identify["Linux"] = tts_linux
-
-    #file_to_generate = "french_revolution.txt"
-    file_to_generate = "ten_days.txt"
-    file_path = Path(file_to_generate)
-    txt_file = file_path.with_suffix(".txt")
-
-    file_type = Path(file_to_generate).suffix
-    for key, value in convert_to_txt.items():
-        if file_type == key:
-            if callable(value):
-                txt_file = value(file_to_generate)
-
-    system = platform.system()
-    print("system",system)
-    for key, value in os_identify.items():
-        if system == key:
-            #if callable(value):
-            value(txt_file)
 
 
+def pdf_to_txt(pdf):
+    from pypdf import PdfReader
+    #pdf="file.pdf"
+    reader=PdfReader(pdf)
+    text=""
+    count = 0
+    for page in reader.pages:
+        count=count+1
+        print("getting text from",pdf,"page",str(count),"of",len(reader.pages))
+        text+=page.extract_text()+"\n"
+    print(text)
+    output_file = pdf.replace(".pdf",".txt")
+    with open(output_file,"w",encoding="utf-8") as f:
+        f.write(text)
+    print("generated",output_file)
+""" 
 
+"""
 def tts_win7(txt_file):
     minutes_per_file = 60
     text = read_txt(txt_file)
@@ -109,6 +177,7 @@ def tts_linux(txt_file):
 
 def txt_to_txt(txt_file):
     return txt_file
+
 def epub_to_txt(epub_file):
     #epub_to_txt("french_revolution.epub")
     import epub2txt
@@ -118,6 +187,7 @@ def epub_to_txt(epub_file):
         f.write(text_content)
     print(f"Successfully converted {epub_file} to {txt_file}!")
     return txt_file
+    
 def pdf_to_txt(pdf_file):
     import fitz
     txt_file = pdf_file.replace(".pdf",".txt")
@@ -132,5 +202,5 @@ def pdf_to_txt(pdf_file):
 #def epub_to_txt():
 #    blank = "blank"
 
-
+"""
 main()
