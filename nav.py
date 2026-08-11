47fcd0e17e4acc99f741b9d537c8e8ecebfb55bf
violin788788 <oak234345@gmail.com>
2026-08-11 17:08:12 -0400

    sync 2026-08-11_17-08-12

--- nav.py
+++ nav.py
@@ -15,6 +15,7 @@ import subprocess,time
 
 chrome_32_or_64_bit = 32
 firefox_32_or_64_bit = 32
+drive = "B:"
 
 match = []
 match.append([32,"Program Files (x86)"])
@@ -31,6 +32,11 @@ print("firefox_program_files",firefox_program_files)
 #chrome_location = "B:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
 chrome_location = drive+"\\"+chrome_program_files+"\\Google\\Chrome\\Application\\chrome.exe"
 firefox_location = drive+"\\"+firefox_program_files+"\\Mozilla Firefox\\private_browsing.exe"
+
+print("chrome_location",chrome_location)
+print("firefox_location",firefox_location)
+#sys.exit()
+
 #chrome_path = os.path.join(os.path.splitdrive(os.getcwd())[0] + "\\", "Program Files (x86)", "Google", "Chrome", "Application", "chrome.exe")
 #print ("chrome path = ",chrome_path)
 url = "https://info34.pythonanywhere.com"
