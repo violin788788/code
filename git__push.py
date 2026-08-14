71e7cec8ba137ef6ba2c6e53e5adbb3e0984e414
violin788788 <oak234345@gmail.com>
2026-08-14 06:47:26 -0400

    sync 2026-08-14_06-47-26

--- git__push.py
+++ git__push.py
@@ -35,42 +35,11 @@ except subprocess.CalledProcessError as e:
 if system_type=="Linux":
     print("Linux detected - not opening Chrome.")
     sys.exit()
-
-
-chrome_32_or_64_bit = 32
-firefox_32_or_64_bit = 64
-drive = "B:"
-
-match = []
-match.append([32,"Program Files (x86)"])
-match.append([64,"Program Files"])
-chrome_program_files = ""
-firefox_program_files = ""
-for a in range(0,len(match)):
-    if chrome_32_or_64_bit==match[a][0]:
-        chrome_program_files = match[a][1]
-    if firefox_32_or_64_bit==match[a][0]:
-        firefox_program_files = match[a][1]
-print("chrome_program_files",chrome_program_files)
-print("firefox_program_files",firefox_program_files)
-#chrome_location = "B:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-chrome_location = drive+"\\"+chrome_program_files+"\\Google\\Chrome\\Application\\chrome.exe"
-firefox_location = drive+"\\"+firefox_program_files+"\\Mozilla Firefox\\private_browsing.exe"
-
-
 github_repo=f"https://github.com/violin788788/{directory_name}"
 python_anywhere_console = "https://www.pythonanywhere.com/user/info34/consoles/"
-#if os.path.exists(chrome_path):
-#subprocess.run([chrome_location,"--incognito",github_repo])
-#subprocess.run([chrome_location,"--incognito",python_anywhere_console])
-#subprocess.run([firefox_location,github_repo])
-#subprocess.run([firefox_location,python_anywhere_console])
 
-print("firefox_location",firefox_location)
-
-firefox_location = "B:\\Program Files\\Mozilla Firefox\\private_browsing.exe"
-
-subprocess.Popen([firefox_location, "--private-window", python_anywhere_console])
+open_browser_to_url("firefox", 64,2,github_repo)
+open_browser_to_url("firefox", 64,2,python_anywhere_console)
 
 print("")
 print("")
