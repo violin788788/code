66fd896c72e7875a9a60bbd4aed9ed5c28f7d31a
violin788788 <oak234345@gmail.com>
2026-08-15 09:54:38 -0400

    sync 2026-08-15_09-54-38

--- nav.py
+++ nav.py
@@ -1,4 +1,9 @@
+
+import sys
+sys.path.insert(0, r"A:\\Users\\-\\code")
 from utils import *
+from pathlib import Path
+
 def show(value):
     #show(epub_file)
     for name, val in globals().items():
@@ -12,8 +17,40 @@ cwd = os.getcwd()
 files = os.listdir(cwd)
 import os,pyautogui,pyperclip
 import subprocess,time
-
-open_browser_to_url("firefox", 64,2,"https://info34.pythonanywhere.com/")
+open_browser_to_url("firefox", 64,3,"https://info34.pythonanywhere.com/")
 pyautogui.hotkey("win","left")    
-open_browser_to_url("firefox", 64,2,"https://info34.pythonanywhere.com/")
+open_browser_to_url("firefox", 64,3,"https://info34.pythonanywhere.com/")
 pyautogui.hotkey("win","right")    
+
+pyautogui.hotkey("ctrl","f")
+pyautogui.write("browse")    
+click_buttons(["esc","tab","enter"])
+time.sleep(3)
+
+google_username = "oak234345"
+google_password = "cats2534"
+paste_text(google_username)
+pyautogui.press('enter')   
+time.sleep(5)
+paste_text(google_password)
+pyautogui.press('pagedown') 
+
+facebook_username = "violin788788@proton.me"
+facebook_password = "Viovio3#"
+pyautogui.press('tab')   
+paste_text(facebook_username)
+pyautogui.press('tab')  
+paste_text(facebook_password) 
+pyautogui.press('enter')
+pyautogui.press('pagedown') 
+
+
+
+navigate_to_window("Google Accounts")
+
+pyautogui.write("oak234345") 
+click_buttons(["enter"]) 
+
+pyperclip.copy("oak234345")
+pyautogui.hotkey("ctrl", "v")  
+click_buttons(["enter"]) 
--- utils.py
+++ utils.py
@@ -45,3 +45,39 @@ def open_browser_to_url(browser, bits,wait_time,url):
     pyautogui.hotkey("alt", "d")    
     pyautogui.hotkey("ctrl", "v")  
     pyautogui.press('enter')   
+
+def click_buttons(array):
+    #click_buttons(["esc","tab","enter"])
+    for a in range(0,len(array)):
+       pyautogui.press(array[a])  
+
+def print_array(array):
+    #print_array(array)
+    for a,val in enumerate(array):
+        print(a,val)
+
+
+def navigate_to_window(window_contains):
+    import win32gui
+    import win32con
+    #navigate_to_window("Google Accounts")
+    windows = []
+    def enum_handler(hwnd, ctx):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd):
+            windows.append((hwnd, win32gui.GetWindowText(hwnd)))
+    win32gui.EnumWindows(enum_handler, None)
+    for hwnd, title in windows:
+        if window_contains in title:
+            print(f"\nSwitching to: {title} (Handle: {hwnd})")
+            # Restore window if minimized
+            win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
+            # Bring to foreground
+            win32gui.SetForegroundWindow(hwnd)
+            break
+
+
+def paste_text(text):
+    #paste_text("meow")
+    pyperclip.copy(text)
+    pyautogui.hotkey("ctrl", "v")  
+    #pyautogui.press('enter') 
