8caf5f2380a9f85016b4a158b0afa88d8f131c1d
violin788788 <oak234345@gmail.com>
2026-09-01 14:22:25 -0400

    sync 2026-09-01_14-22-25

--- div/faceswap-img.py
+++ div/faceswap-img.py
@@ -1,3 +1,5 @@
+print("usually takes about 20 seconds to finish")
+print("")
 import os,time
 from pathlib import Path
 os.environ["OMP_NUM_THREADS"]="1"
@@ -12,19 +14,20 @@ app=FaceAnalysis(name="buffalo_l",providers=["CPUExecutionProvider"])
 app.prepare(ctx_id=0)
 #original photo
 
-original_photo="lenin.png"
-switch_with = "sanders.png"
+original_photo="che55.jpg"
+switch_with = "sanders.jpg"
 
 target=cv2.imread(original_photo)
 source=cv2.imread(switch_with)
+
 #save_file = "result.png"
 save_file = Path(original_photo).stem+"-"+switch_with
-if source is None: raise Exception("source.jpg not found")
-if target is None: raise Exception("target.jpg not found")
+if source is None: raise Exception(original_photo," not found")
+if target is None: raise Exception(switch_with," not found")
 source_faces=app.get(source)
 target_faces=app.get(target)
-if not source_faces: raise Exception("No face found in source.jpg")
-if not target_faces: raise Exception("No face found in target.jpg")
+if not source_faces: raise Exception("No face found in ",original_photo)
+if not target_faces: raise Exception("No face found in ",switch_with)
 swapper=insightface.model_zoo.get_model("inswapper_128.onnx",providers=["CPUExecutionProvider"])
 #swapper=insightface.model_zoo.get_model("inswapper_128.onnx",providers=["CPUExecutionProvider"],provider_options=[{"arena_extend_strategy":"kSameAsRequested"}])
 result=swapper.get(target,target_faces[0],source_faces[0],paste_back=True)
